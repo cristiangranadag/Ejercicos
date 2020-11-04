489d22e2643c58635584f3403123475692aefe0c
Cristian Granada <cristiansordo11@gmail.com>
2020-11-04 13:33:26 -0500

    Ejercicio javascript

--- Ejercicio3/Ejercicio3.js
+++ Ejercicio3/Ejercicio3.js
@@ -1,1 +1,10 @@
-Ejercicio3
+let anos;
+let anoNaci;
+
+anoNaci = Number(prompt("Ingrese su año de nacimiento: "));
+
+anos = 2020 - anoNaci;
+
+alert("Usted tiene "+anos+" años");
+document.write("Usted tiene "+anos+" años");
+console.log("Usted tiene "+anos+" años")
