c95798933e7dba8cb8b74567cac8d0abae282389
Cristian Granada <cristiansordo11@gmail.com>
2020-11-04 22:12:49 -0500

    Ejercicio Javascript

---
+++ Ejercicio1/Ejercicio1.js
@@ -0,0 +1,12 @@
+let n1;
+let n2;
+let suma;
+
+n1 = Number(prompt("Ingrese un numero: "));
+n2 = Number(prompt("Ingrese otro numero: "));
+
+suma = n1 + n2;
+
+alert("La suma es "+suma);
+console.log("La suma es "+suma);
+document.write("La suma es "+suma);
---
+++ Ejercicio2/Ejercicio2.js
@@ -0,0 +1,9 @@
+let num1 = 5;
+let num2 = 10;
+let resultado;
+
+resultado = num1 * num2;
+
+alert("La multiplicacion es "+resultado);
+console.log("La multiplicacion es "+resultado);
+document.write("La multiplicacion es "+resultado);
