d73d7580529f5274ee24d8df984befb677208a9a
Cristian Granada <cristiansordo11@gmail.com>
2020-11-04 20:03:11 -0500

    Ejercicios JS

--- Ejercicio4/Ejercicio4.js
+++ Ejercicio4/Ejercicio4.js
@@ -1,1 +1,10 @@
-Ejercicio4
+let n1 = 4.3;
+let n2 = 3.5;
+let n3 = 2.8;
+let promedio;
+
+promedio = (n1+n2+n3)/3;
+
+alert("El promedio de 3 notas "+promedio);
+console.log("El promedio de 3 notas "+promedio);
+document.write("El promedio de 3 notas "+promedio);
